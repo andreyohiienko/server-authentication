f8a2a033956b40d931d22ec5ffc36f46cdb76bff
Andrei Ohiienko <andreyohiienko@gmail.com>
2020-07-15 01:22:26 +0300

    Express middleware

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const morgan = require('morgan')
 const app = express()
 
 // App Setup
+app.use(morgan('combined'))
+app.use(bodyParser.json({ type: '*/*' }))
 
 // Server Setup
-
 const port = process.env.PORT || 3090
 const server = http.createServer(app)
 server.listen(port)
